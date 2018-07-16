4175c189cb810c8c035ff22b317bd3478ddba976
Pavel <3070799pp@gmail.com>
2018-07-17 00:00:36 +0300

    Task_8_9

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,30 @@ public class Main {
 
         TasksArrays tasksArrays = new TasksArrays();
 
-        tasksArrays.fill(new int[10],5);
-
-        tasksArrays.sum(new int[]{8,15,89,6});
-
-        tasksArrays.findFirstPositive(new int[]{-2,15,0,2,-85});
-        tasksArrays.findLastPositive(new int[]{-2,-15,0,-2,-85});
-
-        tasksArrays.countPositives(new int[]{3,-2,-1,0,1,2,3,4,5,6});
-
-        tasksArrays.printMatrix(new int[][]{{1,2,3,4},
-                                            {1,2,3,4},
-                                            {1,2,3,4},
-                                            {1,2,3,4}});
-
-        tasksArrays.calculateAverage(new int[][]{{1,2,3,4},
-                                                    {5,6,7,8},
-                                                    {9,0,1,2}});
+//        tasksArrays.fill(new int[10],5);
+//
+//        tasksArrays.sum(new int[]{8,15,89,6});
+//
+//        tasksArrays.findFirstPositive(new int[]{-2,15,0,2,-85});
+//        tasksArrays.findLastPositive(new int[]{-2,-15,0,-2,-85});
+//
+//        tasksArrays.countPositives(new int[]{3,-2,-1,0,1,2,3,4,5,6});
+//
+//        tasksArrays.printMatrix(new int[][]{{1,2,3,4},
+//                                            {1,2,3,4},
+//                                            {1,2,3,4},
+//                                            {1,2,3,4}});
+//
+//        tasksArrays.calculateAverage(new int[][]{{1,2,3,4},
+//                                                    {5,6,7,8},
+//                                                    {9,0,1,2}});
+//        tasksArrays.findMaxSumRow(new int[][]{{1,2,3,4},
+//                                                {5,6,7,8},
+//                                                {9,0,1,2},
+//                                                {7,8,0,0}});
+        tasksArrays.transpose(new int[][]{{1,2,3,4},
+                                            {5,6,7,8},
+                                            {9,0,1,2}});
 
     }
 }
--- src/TasksArrays.java
+++ src/TasksArrays.java
@@ -2,33 +2,35 @@ import java.util.Arrays;
 
 public class TasksArrays {
     //Task 1* (ArrayFiller)
-    void fill(int[] array, int filler){
-        for (int i = 0; i < array.length ; i++) {
+    void fill(int[] array, int filler) {
+        for (int i = 0; i < array.length; i++) {
             array[i] = filler;
         }
         System.out.println(Arrays.toString(array));
     }
+
     //Task 2* (ArraySumCalculator)
-    int sum(int[] array){
+    int sum(int[] array) {
         int sumArray = 0;
-        for (int i = 0; i < array.length ; i++) {
+        for (int i = 0; i < array.length; i++) {
             sumArray += array[i];
         }
         System.out.println("sum: " + sumArray);
         return sumArray;
     }
+
     //Task 3* (ArrayPositiveFinder)
-    int findFirstPositive(int[] array){
+    int findFirstPositive(int[] array) {
         int first;
-    int firstElement = -1;
-        for (int i = 0; i <array.length ; i++) {
-            if (array[i] > 0 ){
+        int firstElement = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
                 first = array[i];
-                for (int j = 0; j <array.length ; j++) {
-                    if (array[j]>0 && array[j]<first){
+                for (int j = 0; j < array.length; j++) {
+                    if (array[j] > 0 && array[j] < first) {
                         first = array[j];
                         firstElement = j;
-                    }else if (array[j] == first){
+                    } else if (array[j] == first) {
                         firstElement = j;
                     }
                 }
@@ -37,64 +39,106 @@ public class TasksArrays {
         System.out.println("findFirstPositive: " + firstElement);
         return firstElement;
     }
+
     //Task 4* (ArrayPositivesCounter)
-    int findLastPositive(int[] array){
+    int findLastPositive(int[] array) {
         int last;
         int lastElement = -1;
-        for (int i = 0; i <array.length ; i++) {
-            if (array[i]>0){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
                 last = array[i];
-                for (int j = 0; j <array.length ; j++) {
-                    if (array[j]>0 && array[j]>last){
+                for (int j = 0; j < array.length; j++) {
+                    if (array[j] > 0 && array[j] > last) {
                         last = array[j];
                         lastElement = j;
-                }
+                    }
                 }
             }
         }
         System.out.println("findLastPositive: " + lastElement);
         return lastElement;
     }
+
     //Task 5* (SequenceArrayCreator)
-    int countPositives(int[] array){
+    int countPositives(int[] array) {
         int count = 0;
-        for (int i = 0; i <array.length ; i++) {
-            if (array[i]>0){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
                 count++;
             }
         }
         System.out.println(count);
         return count;
     }
+
     //Task 6* (MatrixPrinter)
-    void printMatrix(int[][] matrix){
-        for (int i = 0; i <matrix.length ; i++) {
+    void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
             System.out.println(Arrays.toString(matrix[i]));
         }
     }
+
     //Task 7* (MatrixAverageCalculator)
-    double calculateAverage(int[][] matrix){
+    double calculateAverage(int[][] matrix) {
         int sum = 0;
         int amountNumbers = 0;
         double count;
-        for (int i = 0; i <matrix.length ; i++) {
-            for (int j = 0; j <matrix[i].length ; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 sum += matrix[i][j];
             }
             amountNumbers += matrix[i].length;
         }
-        count = sum/amountNumbers;
+        count = sum / amountNumbers;
         System.out.println("calculateAverage: " + count);
         return count;
     }
-    //Task 8* (MatrixMaxSumRowFinder)
-//    int findMaxSumRow(int[][] matrix){
-//        int index = -1;
-//        int sum;
-//        for (int i = 0; i <matrix.length ; i++) {
-//
-//        }
-//        return index;
-//    }
 
+    //Task 8* (MatrixMaxSumRowFinder)
+    int findMaxSumRow(int[][] matrix) {
+        int index = -1;
+        int sum;
+        int[] sumArr = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            sum = 0;
+            for (int j = 0; j < matrix[i].length; j++) {
+                sum += matrix[i][j];
+            }
+            sumArr[i] = sum;
+        }
+        int max = sumArr[0];
+        for (int i = 0; i < sumArr.length; i++) {
+            if (sumArr[i] > max) {
+                max = sumArr[i];
+                index = i;
+            }
+        }
+
+        System.out.println("findMaxSumRow: " + index);
+        return index;
+    }
+
+    //Task 9* (MatrixTransposer)
+    int[][] transpose(int[][] sourceMatrix) {
+        int x = 0;
+        int y = sourceMatrix[0].length;
+        for (int[] array : sourceMatrix) {
+            x++;
+        }
+        int[][] transposeMatrix = new int[y][x];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                transposeMatrix[j][i] = sourceMatrix[i][j];
+            }
+        }
+        for (int[] array : sourceMatrix) {
+            System.out.println(Arrays.toString(array));
+        }
+        System.out.println("   ");
+        for (int[] array : transposeMatrix) {
+            System.out.println(Arrays.toString(array));
+        }
+        return transposeMatrix;
+    }
 }
+
