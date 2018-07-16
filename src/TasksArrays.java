0edbe3a157762baa71dceb231184a3597bce8e6e
Pavel <3070799pp@gmail.com>
2018-07-16 15:06:23 +0300

    Task_3_ArrayPositiveFinder

--- src/TasksArrays.java
+++ src/TasksArrays.java
@@ -17,4 +17,44 @@ public class TasksArrays {
         System.out.println("sum: " + sumArray);
         return sumArray;
     }
+
+    int findFirstPositive(int[] array){
+        int first;
+    int firstElement = -1;
+        for (int i = 0; i <array.length ; i++) {
+            if (array[i] > 0 ){
+                first = array[i];
+                for (int j = 0; j <array.length ; j++) {
+                    if (array[j]>0 && array[j]<first){
+                        first = array[j];
+                        firstElement = j;
+                    }else if (array[j] == first){
+                        firstElement = j;
+                    }
+                }
+            }
+        }
+        System.out.println("findFirstPositive: " + firstElement);
+        return firstElement;
+    }
+
+    int findLastPositive(int[] array){
+        int last;
+        int lastElement = -1;
+        for (int i = 0; i <array.length ; i++) {
+            if (array[i]>0){
+                last = array[i];
+                for (int j = 0; j <array.length ; j++) {
+                    if (array[j]>0 && array[j]>last){
+                        last = array[j];
+                        lastElement = j;
+                }
+                }
+            }
+        }
+        System.out.println("findLastPositive: " + lastElement);
+        System.out.println();
+        return lastElement;
+    }
+
 }
