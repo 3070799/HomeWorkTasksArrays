ce45d40749282cdfe5fb4f5157b5c79842064af1
Pavel <3070799pp@gmail.com>
2018-07-19 02:55:36 +0300

    TasksArraysLvl2_Task2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         TasksArrays tasksArrays = new TasksArrays();
         TasksArrays2 tasksArrays2 = new TasksArrays2();
         TasksArraysLvl2 tasksArraysLvl2 = new TasksArraysLvl2();
 
-        tasksArraysLvl2.cyclicShift(new int[]{0, 1, 2, 3, 4}, 5);
+        tasksArraysLvl2.createSpiral(sc.nextInt(),sc.nextInt());
     }
 
 }
--- src/TasksArraysLvl2.java
+++ src/TasksArraysLvl2.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TasksArraysLvl2 {
     //Task 1** (FrequentElemFinder)
@@ -60,5 +61,81 @@ public class TasksArraysLvl2 {
         }
         System.out.println(Arrays.toString(array1));
     }
-    
+
+    //Task 3** (SpiralCreator)
+    //Написать метод создания матрицы размера rows*cols, заполненной в виде спирали, начиная с 1. Если rows, cols – отрицательные, бросить исключение IllegalArgumentException.
+    //int[][] createSpiral(int rows, int cols)
+    //
+    //Пример работы:
+    //createSpiral(3, 3) => {	{1, 2, 3},
+    //{8, 9, 4},
+    //{7, 6, 5}	}
+    //createSpiral(2, 4) => {	{1, 2, 3, 4},
+    //{8, 7, 6, 5}	}
+    //createSpiral(-1, 1)  IllegalArgumentException
+    int[][] createSpiral(int rows, int cols){
+        Scanner sc = new Scanner(System.in);
+        int[][] array = new int[rows][cols];
+        System.out.println("Please enter your peremenuy x");
+        int x = sc.nextInt();
+        int x1 = x-1;
+        for (int i = 0; i < cols ; i++) {
+            array[0][i] = x;
+            x++;
+        }
+        for (int i = 1; i < rows ; i++) {
+            array[i][cols-1] = x;
+            x++;
+        }
+        for (int i = cols -2; i >= 0  ; i--) {
+            array[rows-1][i] = x;
+            x++;
+        }
+        for (int i = rows - 2; i > 0 ; i--) {
+            array[i][0] = x;
+            x++;
+        }
+        int c = 1;
+        int d = 1;
+        while (x < rows * cols + x1) {
+            //Движемся вправо.
+            while (array[c][d + 1] == 0) {
+                array[c][d] = x;
+                x++;
+                d++;
+            }
+
+            //Движемся вниз.
+            while (array[c + 1][d] == 0) {
+                array[c][d] = x;
+                x++;
+                c++;
+            }
+
+            //Движемся влево.
+            while (array[c][d - 1] == 0) {
+                array[c][d] = x;
+                x++;
+                d--;
+            }
+
+            //Движемся вверх.
+            while (array[c - 1][d] == 0) {
+                array[c][d] = x;
+                x++;
+                c--;
+            }
+        }
+        array[c][d] = x;
+
+
+
+
+
+        for (int i = 0; i < array.length ; i++) {
+            System.out.println(Arrays.toString(array[i]));
+        }
+
+        return array;
+    }
 }
